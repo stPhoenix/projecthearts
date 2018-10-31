a4dbbd2a903a30f44ed0fbe4b4b7d25a82f6d238
StPhoenix <bohdansapovsky@gmail.com>
2018-10-31 21:08:17 +0200

    Init commit

---
+++ build/precache-manifest.26bdbf41def77eb80d9842e67ee3a7f4.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = [
+  {
+    "revision": "a46a54d93ff6e543497d205db97fd460",
+    "url": "/static/media/8C.a46a54d9.png"
+  },
+  {
+    "revision": "0d104ceb45b7ca3acfe4",
+    "url": "/static/css/main.6e2f7633.chunk.css"
+  },
+  {
+    "revision": "d05b85f268c0a03a1f5a89bc8806bf11",
+    "url": "/static/media/bg.d05b85f2.jpg"
+  },
+  {
+    "revision": "024100091ddb6e2d7127",
+    "url": "/static/js/1.02410009.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "136dab439961d61f4b9c1f939e07828d",
+    "url": "/static/media/2C.136dab43.png"
+  },
+  {
+    "revision": "1451de5d494406e395215276a2489a7c",
+    "url": "/static/media/2D.1451de5d.png"
+  },
+  {
+    "revision": "ebe434953cb5c82ec5c8e38b8e735489",
+    "url": "/static/media/2H.ebe43495.png"
+  },
+  {
+    "revision": "33c964ae685ff65233d2ac2c35b96072",
+    "url": "/static/media/2S.33c964ae.png"
+  },
+  {
+    "revision": "6ed5bf64f008d72e83c67f81468ab7c5",
+    "url": "/static/media/3C.6ed5bf64.png"
+  },
+  {
+    "revision": "690d5a477dfe9ed93261bd814c0a40d6",
+    "url": "/static/media/3D.690d5a47.png"
+  },
+  {
+    "revision": "7278d51dda50bd4b8c8cadb7fe349953",
+    "url": "/static/media/3H.7278d51d.png"
+  },
+  {
+    "revision": "9638bf9f29919b027765992a81f6d39b",
+    "url": "/static/media/3S.9638bf9f.png"
+  },
+  {
+    "revision": "23c51e0b449e342f8b7f6c8fd57e2137",
+    "url": "/static/media/4C.23c51e0b.png"
+  },
+  {
+    "revision": "89eaac32133444fe40876cb51f697f13",
+    "url": "/static/media/4D.89eaac32.png"
+  },
+  {
+    "revision": "6695f1d514765572dfe7e5dd0863dfce",
+    "url": "/static/media/4H.6695f1d5.png"
+  },
+  {
+    "revision": "5156259e1f30dece1376dc5695a9a1d4",
+    "url": "/static/media/4S.5156259e.png"
+  },
+  {
+    "revision": "3ac88eddd1ac03bfc901de76424b5aba",
+    "url": "/static/media/5C.3ac88edd.png"
+  },
+  {
+    "revision": "24fd440cbb52affc5242a507c9dec4d1",
+    "url": "/static/media/5D.24fd440c.png"
+  },
+  {
+    "revision": "5457f189cabc0476511c7ed1421b419d",
+    "url": "/static/media/5H.5457f189.png"
+  },
+  {
+    "revision": "5ab3d8627d0c4d17c86c73d90817900b",
+    "url": "/static/media/5S.5ab3d862.png"
+  },
+  {
+    "revision": "5c96f40f637a6b2ed34d5e837d81ef7b",
+    "url": "/static/media/6C.5c96f40f.png"
+  },
+  {
+    "revision": "ff1c62992cf49b6164401d1d9f77a20c",
+    "url": "/static/media/6D.ff1c6299.png"
+  },
+  {
+    "revision": "84564e31bf0a0e8bcd5fdd3d4aa12cc3",
+    "url": "/static/media/6H.84564e31.png"
+  },
+  {
+    "revision": "c99b0f04000841e5f3db53d64b3f7034",
+    "url": "/static/media/6S.c99b0f04.png"
+  },
+  {
+    "revision": "4808cc416c976dbd5c3cb629a0f102a5",
+    "url": "/static/media/7C.4808cc41.png"
+  },
+  {
+    "revision": "58d872f063752027e59e15af4d090123",
+    "url": "/static/media/7D.58d872f0.png"
+  },
+  {
+    "revision": "c8475b530ea9f0970ff829c97f3e7abf",
+    "url": "/static/media/7H.c8475b53.png"
+  },
+  {
+    "revision": "82d62864ff67ff7b5e20150316d26872",
+    "url": "/static/media/7S.82d62864.png"
+  },
+  {
+    "revision": "0d104ceb45b7ca3acfe4",
+    "url": "/static/js/main.0d104ceb.chunk.js"
+  },
+  {
+    "revision": "fcafeaf2d610109aa1916208080ab711",
+    "url": "/static/media/8D.fcafeaf2.png"
+  },
+  {
+    "revision": "1e9e59b0d6f684908f3ad7d2fe942a42",
+    "url": "/static/media/8H.1e9e59b0.png"
+  },
+  {
+    "revision": "497eed1efa7e554de5b62bc9eb2b5ae6",
+    "url": "/static/media/8S.497eed1e.png"
+  },
+  {
+    "revision": "4f8d0d8f2ebaeacf99e7134221d6da49",
+    "url": "/static/media/9C.4f8d0d8f.png"
+  },
+  {
+    "revision": "1d495bb1ebf632dca66a538b61bae19a",
+    "url": "/static/media/9D.1d495bb1.png"
+  },
+  {
+    "revision": "33ab7891b4259ea8d8823a352ef38cdf",
+    "url": "/static/media/9H.33ab7891.png"
+  },
+  {
+    "revision": "50caac183ef66afe6cdda1d2e3daafda",
+    "url": "/static/media/9S.50caac18.png"
+  },
+  {
+    "revision": "1963502749c024e1590789bab3632bc9",
+    "url": "/static/media/10C.19635027.png"
+  },
+  {
+    "revision": "39ef422e15761450c33f8cb3ee369ebe",
+    "url": "/static/media/10D.39ef422e.png"
+  },
+  {
+    "revision": "60ab387c8c1c50dd3a75255500218b94",
+    "url": "/static/media/10H.60ab387c.png"
+  },
+  {
+    "revision": "b208cd7f21ce87cf950936ab6fb3e717",
+    "url": "/static/media/10S.b208cd7f.png"
+  },
+  {
+    "revision": "17ca9f77b395a41c6d45cbff350573bb",
+    "url": "/static/media/AC.17ca9f77.png"
+  },
+  {
+    "revision": "f37d608fb18056bc4f83737d633c5cf5",
+    "url": "/static/media/AD.f37d608f.png"
+  },
+  {
+    "revision": "a762540d55018bcf43ffba8b6343c9c9",
+    "url": "/static/media/AH.a762540d.png"
+  },
+  {
+    "revision": "e6a6d2f8a350079dcad97e814d3559e7",
+    "url": "/static/media/AS.e6a6d2f8.png"
+  },
+  {
+    "revision": "1595a3ea21f452c290cf26af7ae53ede",
+    "url": "/static/media/JC.1595a3ea.png"
+  },
+  {
+    "revision": "d5682eb454510b9cdf54beb9c5a0e5f5",
+    "url": "/static/media/JD.d5682eb4.png"
+  },
+  {
+    "revision": "fdf4ffc434ac567c6b77905cb1b11c8a",
+    "url": "/static/media/JH.fdf4ffc4.png"
+  },
+  {
+    "revision": "89d2c97e62fcaec36c80dbdd725f216f",
+    "url": "/static/media/JS.89d2c97e.png"
+  },
+  {
+    "revision": "aa73cfd0e548d87f4af4a18a8d2c3c8f",
+    "url": "/static/media/KC.aa73cfd0.png"
+  },
+  {
+    "revision": "6b5b30c00309e3cfae486f543bc1809a",
+    "url": "/static/media/KD.6b5b30c0.png"
+  },
+  {
+    "revision": "3b48ae8207b07a1f9477d7e6135ca211",
+    "url": "/static/media/KH.3b48ae82.png"
+  },
+  {
+    "revision": "e8d1a4cfe9d8dca64733c050e65e9dfd",
+    "url": "/static/media/KS.e8d1a4cf.png"
+  },
+  {
+    "revision": "d6296277472d7e9594d0ebee0cb82cdc",
+    "url": "/static/media/QC.d6296277.png"
+  },
+  {
+    "revision": "6940967da8669e3410fefd3a8a33aad9",
+    "url": "/static/media/QD.6940967d.png"
+  },
+  {
+    "revision": "63afa62b47efa84b8b87d3e39ef42bc1",
+    "url": "/static/media/QH.63afa62b.png"
+  },
+  {
+    "revision": "9026849bcbad6fd92f7c3cb8f70e3cd4",
+    "url": "/static/media/QS.9026849b.png"
+  },
+  {
+    "revision": "024100091ddb6e2d7127",
+    "url": "/static/css/1.68ef65f0.chunk.css"
+  },
+  {
+    "revision": "461017ae4add795ed65e931002ccb662",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,26 +1,119 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import C2 from './PNG/2C.png';
+import D2 from './PNG/2D.png';
+import H2 from './PNG/2H.png';
+import S2 from './PNG/2S.png';
+import C3 from './PNG/3C.png';
+import D3 from './PNG/3D.png';
+import H3 from './PNG/3H.png';
+import S3 from './PNG/3S.png';
+import C4 from './PNG/4C.png';
+import D4 from './PNG/4D.png';
+import H4 from './PNG/4H.png';
+import S4 from './PNG/4S.png';
+import C5 from './PNG/5C.png';
+import D5 from './PNG/5D.png';
+import H5 from './PNG/5H.png';
+import S5 from './PNG/5S.png';
+import C6 from './PNG/6C.png';
+import D6 from './PNG/6D.png';
+import H6 from './PNG/6H.png';
+import S6 from './PNG/6S.png';
+import C7 from './PNG/7C.png';
+import D7 from './PNG/7D.png';
+import H7 from './PNG/7H.png';
+import S7 from './PNG/7S.png';
+import C8 from './PNG/8C.png';
+import D8 from './PNG/8D.png';
+import H8 from './PNG/8H.png';
+import S8 from './PNG/8S.png';
+import C9 from './PNG/9C.png';
+import D9 from './PNG/9D.png';
+import H9 from './PNG/9H.png';
+import S9 from './PNG/9S.png';
+import C10 from './PNG/10C.png';
+import D10 from './PNG/10D.png';
+import H10 from './PNG/10H.png';
+import S10 from './PNG/10S.png';
+import AC from './PNG/AC.png';
+import AD from './PNG/AD.png';
+import AH from './PNG/AH.png';
+import AS from './PNG/AS.png';
+import JC from './PNG/JC.png';
+import JD from './PNG/JD.png';
+import JH from './PNG/JH.png';
+import JS from './PNG/JS.png';
+import KC from './PNG/KC.png';
+import KD from './PNG/KD.png';
+import KH from './PNG/KH.png';
+import KS from './PNG/KS.png';
+import QC from './PNG/QC.png';
+import QD from './PNG/QD.png';
+import QH from './PNG/QH.png';
+import QS from './PNG/QS.png';
+import { UncontrolledTooltip } from 'reactstrap';
 
+
+const deck = [
+                D2, D3, D4, D5, D6, D7, D8, D9, D10, JD, QD, KD, AD,
+                C2, C3, C4, C5, C6, C7, C8, C9, C10, JC, QC, KC, AC,
+                S2, S3, S4, S5, S6, S7, S8, S9, S10, JS, QS, KS, AS,
+                H2, H3, H4, H5, H6, H7, H8, H9, H10, JH, QH, KH, AH,
+             ];
 class App extends Component {
+  constructor(props){
+      super(props);
+      this.state = {hand: [], hole: [], temp_deck: [], message: 'Press start'};
+      this.start = this.start.bind(this);
+      this.more = this.more.bind(this);
+  }
+  start () {
+      const t_deck = [...deck.sort((a, b) => {return(0.5 - Math.random())})];
+      this.setState({hand: [t_deck.shift(), t_deck.shift(), t_deck.shift()], hole: [t_deck.shift(), t_deck.shift()]});
+      this.setState({temp_deck: [...t_deck], message: 'Press more for adding card'});
+  };
+  more () {
+      if (this.state.hand.length < 5) {
+          const t_deck = [...this.state.temp_deck]
+          this.setState({hand: [...this.state.hand, t_deck.shift()]});
+          this.setState({temp_deck: [...t_deck]});
+      }
+      else{
+          this.setState({message: 'Hand is full. Press start.'})
+      }
+      
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main className="container-fluid align-self-center row d-flex flex-column justify-content-center mx-0">
+        <section className="col-12">
+            <h1 className="d-none d-md-block display-4 text-center text-white ">{this.state.message}</h1>
+            <h1 className="d-md-none text-center text-white ">{this.state.message}</h1>
+        </section>
+        <section className="col-12 d-flex justify-content-center">
+            <ul className="list-unstyled d-flex flex-row justify-content-center">
+                {this.state.hand.map((card, iterator) => {return(<li className="" key={"card"+iterator}><img className="img-fluid mx-2" style={{width:"10rem"}} src={card} alt="" /></li>)})}
+            </ul>
+        </section>
+        <section className="col-12">    
+            <hr />
+        </section>
+        <section className="col-12 d-flex justify-content-center">
+            <ul className="list-unstyled d-flex flex-row justify-content-center">
+                {this.state.hole.map((card, iterator) => {return(<li className="" key={"cardhole"+iterator}><img className="img-fluid mx-2" style={{width:"10rem"}} src={card} alt="" /></li>)})}
+            </ul>
+        </section>
+        <section className="col-12 d-flex fixed-bottom mb-3">
+            <i className="material-icons text-white md-48 " id="UncontrolledTooltipInfo">info
+                 <UncontrolledTooltip placement="top" target="UncontrolledTooltipInfo">
+                    This app can generate poker community and hole cards. Press start to generate flop cards. Press more to generate turn and one more time for river.
+                 </UncontrolledTooltip>
+            </i>
+            <button className="btn btn-outline-white ml-auto mr-1" onClick={this.more}>More</button>
+            <button className="btn btn-outline-white mr-auto ml-1" onClick={this.start}>Start</button>
+        </section>
+      </main>
     );
   }
 }
